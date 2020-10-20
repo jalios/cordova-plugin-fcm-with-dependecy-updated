9a90f179e1ee8cc98faf810c5016905bda70bc32
syrine kriaa <sirine.kriaa@jalios.com>
2020-10-20 12:05:59 +0200

    Add SecurityUtils class that contains the decryption function

---
+++ src/android/com/gae/scaffolder/plugin/SecurityUtils.java
@@ -0,0 +1,33 @@
+package com.gae.scaffolder.plugin;
+
+import android.util.Base64;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+
+public class SecurityUtils {
+
+    public static  String decrypt(String value, String key, String algo, String transformation) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException {
+        byte[] keyBytes = new byte[0];
+        try {
+            keyBytes = key.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        byte[] message = Base64.decode(value, Base64.DEFAULT);
+
+        Cipher cipher = Cipher.getInstance(transformation);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(keyBytes, algo);
+
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        byte[] decrypted = cipher.doFinal(message);
+
+        return new String(decrypted);
+    }
+}
